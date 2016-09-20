056bc00ba7d44d70a197d690086bec96a87895c3
Charles Reid <charlesreid1@gmail.com>
2016-09-20 00:39:13 -0700

    adding base 10 primes list to docs, renaming stylesheet to primes.css

--- base10/PrimesPage.java
+++ base10/PrimesPage.java
@@ -97,7 +97,7 @@ public class PrimesPage {
         System.out.println("");
         System.out.println("    <title>Radix Tables - Prime Numbers</title>");
         System.out.println("");
-        System.out.println("    <link rel=\"stylesheet\" href=\"style.css\">");
+        System.out.println("    <link rel=\"stylesheet\" href=\"primes.css\">");
         System.out.println("");
         System.out.println("    <!-- Latest compiled and minified CSS -->");
         System.out.println("    <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">");
