db08dd1c0412ef44d1d079991ce055d92f6c29b3
Charles Reid <charlesreid1@gmail.com>
2016-09-22 01:53:59 -0700

    simplify the fancy pants animation stuff

--- dynamicPage/js/dynamicpage.js
+++ dynamicPage/js/dynamicpage.js
@@ -22,15 +22,16 @@ $(function() {
             $mainContent
                 .find("#guts")
                 .fadeOut(200, function() {
-                    $mainContent.hide().load(newHash + " #guts", function() {
-                        $mainContent.fadeIn(200, function() {
-                            $pageWrap.animate({
-                                height: baseHeight + $mainContent.height() + "px"
-                            });
-                        });
-                        $("nav a").removeClass("current");
-                        $("nav a[href="+newHash+"]").addClass("current");
-                    });
+                    $mainContent.hide().load(newHash + " #guts");
+                    //, function() {
+                    //    $mainContent.fadeIn(200, function() {
+                    //        $pageWrap.animate({
+                    //            height: baseHeight + $mainContent.height() + "px"
+                    //        });
+                    //    });
+                    //    $("nav a").removeClass("current");
+                    //    $("nav a[href="+newHash+"]").addClass("current");
+                    //});
                 });
         };
         
