3e6468654d44d52b2794c3612b96d2a2f0db947c
Charles Reid <charlesreid1@gmail.com>
2016-09-23 01:08:36 -0700

    Improving layout of prime number table

--- base3/PrimesPage3.java
+++ base3/PrimesPage3.java
@@ -10,9 +10,9 @@ public class PrimesPage3 {
 
         int base = 3;
 
-        int Ncols = 21;
+        int Ncols = 20;
         int Nrows = 100;
-        int n = 1000000;
+        int n = 100000;
         //System.out.println("You'd better have at least "+Ncols*Nrows+" primes. You have "+nprimes);
 
         // initially assume all integers are prime
@@ -52,10 +52,16 @@ public class PrimesPage3 {
         printTableHeader();
 
         // row headers
-        System.out.println("<tr>");
+        System.out.println("<tr class=\"clean floorline\">");
         System.out.println("<td></td>"); // one empty column
         for( int c=0; c < Ncols; c++ ) {
-            System.out.println("<td id=\"fat\">" + (c*100) + "</td>");
+            if(c%5==0) {
+                System.out.println("<td class=\"vtint\" id=\"fat\">");
+            } else {
+                System.out.println("<td id=\"fat\">");
+            }
+            System.out.println( c*100 );
+            System.out.println("</td>");
         }
         System.out.println("</tr>");
         System.out.println();
@@ -66,25 +72,41 @@ public class PrimesPage3 {
         String tempstring = "";
         for(int r=0; r<Nrows; r++) {
 
+            /*
             if(r%5==0) {
                 hasTint = !hasTint;
             }
-
-            if( hasTint ) {
-                System.out.println("<tr id=\"tint\">");
+            if( hasTint ) { 
+                System.out.println("<tr class=\"tint\">");
             } else {
                 System.out.println("<tr>");
             }
+            */
+            System.out.println("<tr>");
 
-            System.out.println("    <td id=\"fat\">" + (r+1) + "</td>"); // prime index
+            System.out.println("    <td class=\"clean\" id=\"fat\">" + (r+1) + "</td>"); // prime index
 
             for(int c=0; c<Ncols; c++) {
                 p = primes.get(c*Nrows + r);
                 temp = new BigInteger( Integer.toString(p) );
                 tempstring = temp.toString(base);
-                System.out.println("    <td>" + tempstring + "</td>");
+                if(c%5==0) {
+                    System.out.print("    <td class=\"vtint\">");
+                } else {
+                    System.out.print("    <td>");
+                }
+                System.out.println(tempstring);
+                System.out.println("</td>");
             }
             System.out.println("</tr>");
+
+            if(r%5==4) { 
+                System.out.println("<tr>");
+                for(int c=0; c<Ncols+1; c++) {
+                    System.out.println("<td>&nbsp;</td>");
+                }
+            }
+
             System.out.println();
         }
 
@@ -94,62 +116,11 @@ public class PrimesPage3 {
     }
 
     public static void printPageHeader() {
-        System.out.println("<!DOCTYPE html>");
-        System.out.println("<html lang=\"en\">");
-        System.out.println("  <head>");
-        System.out.println("    <meta charset=\"utf-8\">");
-        System.out.println("    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">");
-        System.out.println("    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
-        System.out.println("    <meta name=\"description\" content=\"\">");
-        System.out.println("    <meta name=\"author\" content=\"\">");
-        System.out.println("");
-        System.out.println("    <title>Radix Tables - Prime Numbers</title>");
-        System.out.println("");
-        System.out.println("    <link rel=\"stylesheet\" href=\"primes.css\">");
-        System.out.println("");
-        System.out.println("    <!-- Latest compiled and minified CSS -->");
-        System.out.println("    <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\">");
-        System.out.println("    ");
-        System.out.println("    <!-- Optional theme -->");
-        System.out.println("    <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css\" integrity=\"sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp\" crossorigin=\"anonymous\">");
-        System.out.println("");
-        System.out.println("  </head>");
-        System.out.println("");
-        System.out.println("  <body>");
-        System.out.println("    <div class=\"container\">");
-        System.out.println("");
-        System.out.println("        <div class=\"row\">");
-        System.out.println("            <div class=\"col-md-12\">");
-        System.out.println("                <h1>Prime Numbers</h1>");
-        System.out.println("                <p class=\"lead\">The following table lists several thousand of the first prime numbers. ");
-        System.out.println("                Prime numbers are numbers that are evenly divisible only by themselves and 1. </p>");
-        System.out.println("                <p>The primes are listed in column-major order: to look up the 709th prime, ");
-        System.out.println("                find the 700 column, then scroll down to row 9. The 709th prime is 5381. ");
-        System.out.println("                (Coincidentally, 709 is the 127th prime, and 127 is the 31st prime, and 31 is the 11th prime,");
-        System.out.println("                and 11 is the 5th prime, and 5 is the 3rd prime, and 3 is the 2nd prime, ");
-        System.out.println("                and 2 is the 1st prime.)</p>");
-        System.out.println("            </div>");
-        System.out.println("        </div>");
-        System.out.println("        <div class=\"row\">");
-        System.out.println("            <div class=\"col-md-12\">");
-        System.out.println("");
-        System.out.println("<!--");
-        System.out.println("insert table here.");
-        System.out.println("-->");
+        System.out.println("<div id=\"guts\">");
     }
 
     public static void printPageFooter() {
-        System.out.println("<!--");
-        System.out.println("end insert table here.");
-        System.out.println("-->");
-        System.out.println("");
-        System.out.println("            </div>");
-        System.out.println("        </div>");
-        System.out.println("");
-        System.out.println("    </div> <!-- /container -->");
-        System.out.println("");
-        System.out.println("  </body>");
-        System.out.println("</html>");
+        System.out.println("</div><!-- end guts -->");
     }
 
     public static void printTableHeader() {
